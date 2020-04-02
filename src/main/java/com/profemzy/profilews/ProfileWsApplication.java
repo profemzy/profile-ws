0e48e4ac8e1d6a67160bb426c26cebeb7f3f42e5
Femi Oladele <profemzy@gmail.com>
2020-04-02 01:24:21 +0100

    Initial Commit

---
+++ src/main/java/com/profemzy/profilews/ProfileWsApplication.java
@@ -0,0 +1,13 @@
+package com.profemzy.profilews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileWsApplication.class, args);
+    }
+
+}
